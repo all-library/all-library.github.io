289292349842b57ad157a8e0854356f41dd46de2
Arkansas Little Library Organization <61018557+all-library@users.noreply.github.com>
2020-02-18 09:48:31 -0600

    Update code.js

--- bsrc/code.js
+++ bsrc/code.js
@@ -1,11 +1,1 @@
-// Copyright
-var d;
-var dtx; 
-var cyr;
-var con;
-con = document.getElementsByClass("container");
-cyr = Date.getFullYear();
-d = document.createElement("p");
-dtx = "&copy; " + cyr;
-d.innerHTML = dtx;
-con.appendChild(d);
+document.write("&copy; " + Date.getFullYear());
